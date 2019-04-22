9faa9bc425d9549901ad77222d997f16bea36ca7
Ben Hodge <benjamin.andrew.hodge@gmail.com>
2019-04-22 00:07:34 -0500

    code hotswap and upgrade to 0.1.2

--- pragmatic_programmer/concurrency/projects/sequence1/lib/sequence1/server.ex
+++ pragmatic_programmer/concurrency/projects/sequence1/lib/sequence1/server.ex
@@ -1,7 +1,19 @@
 defmodule Sequence1.Server do
   use GenServer
+  require Logger
+  @vsn "1"
 
-  @vsn "0"
+  defmodule State do
+    defstruct current_number: 0, stash_pid: nil, delta: 1
+  end
+
+  def code_change("0", old_state = {current_number, stash_pid}, _extra) do
+    new_state = %State{current_number: current_number, stash_pid: stash_pid, delta: 1}
+    Logger.info("Changing code from 0 to 1")
+    Logger.info(inspect(old_state))
+    Logger.info(inspect(new_state))
+    {:ok, new_state}
+  end
 
   ####
   # External API
@@ -29,26 +41,27 @@ defmodule Sequence1.Server do
 
   def init(stash_pid) do
     current_number = Sequence1.Stash.get_value(stash_pid)
-    {:ok, {current_number, stash_pid}}
+    {:ok, %State{current_number: current_number, stash_pid: stash_pid}}
   end
 
-  def handle_call(:next_number, _from, {current_number, stash_pid}) do
-    {:reply, current_number, {current_number + 1, stash_pid}}
+  def handle_call(:next_number, _from, state) do
+    {:reply, state.current_number,
+     %State{state | current_number: state.current_number + state.delta}}
   end
 
-  def handle_cast({:set_number, new_number}, _from, {_current_number, stash_pid}) do
-    {:noreply, {new_number, stash_pid}}
+  def handle_cast({:set_number, new_number}, _from, state) do
+    {:noreply, %{state | current_number: new_number}}
   end
 
-  def handle_cast({:increment_number, delta}, {current_number, stash_pid}) do
-    {:noreply, {current_number + delta, stash_pid}}
+  def handle_cast({:increment_number, delta}, state) do
+    {:noreply, %{state | current_number: state.current_number + delta, delta: delta}}
   end
 
   def format_status(_reason, [_pdict, state]) do
     [data: [{'State', "My current state is '#{inspect(state)}' and I'm happy!"}]]
   end
 
-  def terminate(_reason, {current_number, stash_pid}) do
-    Sequence1.Stash.save_value(stash_pid, current_number)
+  def terminate(_reason, state) do
+    Sequence1.Stash.save_value(state.stash_pid, state.current_number)
   end
 end
